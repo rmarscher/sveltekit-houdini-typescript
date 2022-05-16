9761af4c773d9d4e50c2f4a5c1ab047df755d1e4
Rob Marscher <rob@robmarscher.com>
2022-05-16 15:16:43 -0400

    Initial commit

---
+++ kit-with-houdini/src/lib/test.ts
@@ -0,0 +1,10 @@
+
+export type Test = {
+  value: string;
+}
+
+export function getTest(): Test {
+  return {
+    value: 'test'
+  }
+}
---
+++ kit-with-houdini/src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { getTest, type Test } from "$lib/test";
+
+  let t: Test = getTest();
+</script>
+
+<div>
+  Testing type imports. {t.value}
+</div>
